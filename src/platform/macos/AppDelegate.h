16bdca83ca16999cb34178caac1d5a0e797a03d3
André Santee <andre.santee@gmail.com>
2018-10-25 13:46:26 -0400

    Base macos project

---
+++ src/platform/macos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sprite
+//
+//  Created by Andre Santee on 25/10/18.
+//  Copyright © 2018 Asantee Games. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ src/platform/macos/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Sprite
+//
+//  Created by Andre Santee on 25/10/18.
+//  Copyright © 2018 Asantee Games. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
